1ba22b647eab605a23e25eda47bfca274482cdeb
Josh Cohen <joshco@gmail.com>
2015-02-07 10:49:59 -0700

    flipper

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $(".image").click(function() {
+   
+   window.setTimeout(rotator,2000);
+
+
+    });
+});
+
+function rotator() {
+	flip_images();
+	window.setTimeout(rotator,2000);
+
+}
+function flip_images() {
+	 	$(".image-flip").fadeToggle();
+}
+function show_image(i) {
+	$(".image-flip")[i].fadeToggle();
+}
